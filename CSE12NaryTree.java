fb354d0792304ebb0eb56afd019b4c4ba25b0e42
MochaCoral <90062849+MochaCoral@users.noreply.github.com>
2022-03-12 19:26:38 -0800

    Initial commit

---
+++ CSE12NaryTree.java
@@ -0,0 +1,126 @@
+/**
+ * TODO: Add file header
+ * Name:
+ * ID:
+ * Email:
+ * File description: 
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * TODO: Add class header
+ */
+public class CSE12NaryTree<E extends Comparable<E>> {
+    
+    /**
+     * This inner class encapsulates the data and children for a Node.
+     * Do NOT edit this inner class.
+     */
+    protected class Node{
+        E data;
+        List<Node> children;
+    
+        /**
+         * Initializes the node with the data passed in
+         * 
+         * @param data The data to initialize the node with
+         */
+        public Node(E data) {
+            this.data = data;
+            this.children = new ArrayList<>();
+        }
+    
+        /**
+         * Getter for data
+         * 
+         * @return Return a reference to data
+         */
+        public E getData() {
+            return data;
+        }
+
+        /**
+         * Setter for the data
+         * 
+         * @param data Data that this node is set to
+         */
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        /**
+         * Getter for children
+         * 
+         * @return reference to the list of children
+         */
+        public List<Node> getChildren() {
+            return children;
+        }
+
+        /**
+         * Returns the number of children
+         * 
+         * @return number of children
+         */
+        public int getNumChildren() {
+            // assume there are no nulls in list
+            return children.size();
+        }
+
+        /**
+         * Add the given node to this node's list of children
+         * 
+         * @param node The node to add
+         */
+        public void addChild(Node node) {
+            children.add(node);
+        }
+    
+    }
+    
+    Node root;
+    int size;
+    int N;
+
+    /**
+     * Constructor that initializes an empty N-ary tree, with the given N
+     * 
+     * @param N The N the N-tree should be initialized with
+     */
+    public CSE12NaryTree(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.root = null;
+        this.size = 0;
+        this.N = N;
+    }
+
+    /**
+     * TODO: Add method header
+     */
+    public void add(E element) {
+        //TODO
+    }
+
+    /**
+     * TODO: Add method header
+     */
+    public boolean contains(E element) {
+        //TODO
+        return false;
+    }
+
+    /**
+     * TODO: Add method header
+     */
+    public ArrayList<E> sortTree(){
+        //TODO
+        return null;
+    }
+}
---
+++ CSE12NaryTreeTester.java
@@ -0,0 +1,48 @@
+/**
+ * TODO: Add file header
+ * Name:
+ * ID:
+ * Email:
+ * File description: 
+ */
+ 
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+/**
+ * TODO: Add class header
+ */
+public class CSE12NaryTreeTester {
+    /**
+     * TODO: Add test case description.
+     */
+    @Test
+    public void testAdd(){
+
+    }
+
+    /**
+     * TODO: Add test case description
+     */
+    @Test
+    public void testContains(){
+        
+    }
+
+    /**
+     * TODO: Add test case description
+     */
+    @Test
+    public void testSortTree(){
+        
+    }
+
+    /**
+     * TODO: Add test case description
+     */
+    @Test
+    public void testCustom(){
+        
+    }
+}
